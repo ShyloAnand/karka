c178f1a22d3e88a93d59394376ff1ec26c8e7fa1
ShyloAnand <147144952+ShyloAnand@users.noreply.github.com>
2025-05-03 12:44:18 +0530

    Add files via upload

---
+++ python full stack/sql/task 1/ 1.sql
@@ -0,0 +1,11 @@
+create database task;
+use task;
+show databases;
+show tables;
+CREATE TABLE students (
+    studentid INT ,
+    name VARCHAR(100),
+    grade varchar(100),
+    age int
+    );
+     select * from students;
