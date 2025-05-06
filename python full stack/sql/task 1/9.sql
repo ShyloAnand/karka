e03168d2e5807d48067427c9aae9cc4aa7dcf9ad
ShyloAnand <147144952+ShyloAnand@users.noreply.github.com>
2025-05-06 12:19:47 +0530

    Add files via upload

---
+++ python full stack/sql/task 1/9.sql
@@ -0,0 +1,11 @@
+create database ask;
+use ask;
+show databases;
+show tables;
+CREATE TABLE employees(
+    empid INT ,
+    empname VARCHAR(100),
+    empsalary int
+    );
+    select * from employees;
+    alter table employees add department varchar(100);
