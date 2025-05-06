7f8930c1108d67840139a480023259a7a3b4a7b6
ShyloAnand <147144952+ShyloAnand@users.noreply.github.com>
2025-05-06 12:26:08 +0530

    Add files via upload

---
+++ python full stack/sql/task 1/10.sql
@@ -0,0 +1,10 @@
+create database sk;
+use sk;
+show databases;
+show tables;
+CREATE TABLE product(
+    productid INT ,
+    productname VARCHAR(100),
+    price int
+    );
+    select * from product;
