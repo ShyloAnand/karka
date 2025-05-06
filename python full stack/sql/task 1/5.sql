1bb147539fc53c5edeb747243c75024c0db2a287
ShyloAnand <147144952+ShyloAnand@users.noreply.github.com>
2025-05-06 12:14:28 +0530

    Add files via upload

---
+++ python full stack/sql/task 1/5.sql
@@ -0,0 +1,14 @@
+create database task;
+use task;
+show databases;
+show tables;
+CREATE TABLE students (
+    studentid INT ,
+    name VARCHAR(100),
+    grade varchar(100),
+    age int
+    );
+     select * from studentdetails;
+     alter table students add email varchar(100);
+     alter table students drop column grade;
+     rename table students to studentdetails;
