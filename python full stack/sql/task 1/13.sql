1a8d48117b79a12d7239736b451306361266aafd
ShyloAnand <147144952+ShyloAnand@users.noreply.github.com>
2025-05-06 15:29:52 +0530

    Add files via upload

---
+++ python full stack/sql/task 1/13.sql
@@ -0,0 +1,25 @@
+create database sk;
+use sk;
+show databases;
+show tables;
+CREATE TABLE books(
+    bookid INT ,
+    title VARCHAR(100),
+    author varchar(100),
+    publish int
+    );
+    select * from books;
+    insert into books values('1','the great gatsby','f.scott fitzg','1925');
+    insert into books values('2','the great s','f.scott','1945');
+	insert into books values('3','the great h','f.scot','1955');
+	insert into books values('4','the great y','f.sco','1965');
+	insert into books values('5','the great l','f.sc','1975');
+	insert into books values('6','the great o','f.s','1985');
+	insert into books values('7','the great w','f.scott king','1995');
+	insert into books values('8','the great e','f.scott a','2005');
+	insert into books values('9','the great l','f.scott ','2015');
+	insert into books values('10','the great l','f.scott w','2025');
+    select * from books;
+    select title,author from books;
+        
+    
