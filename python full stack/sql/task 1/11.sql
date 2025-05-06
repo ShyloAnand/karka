9058700175dad36f7b5ae76f837f1f1db763082a
ShyloAnand <147144952+ShyloAnand@users.noreply.github.com>
2025-05-06 15:27:18 +0530

    Add files via upload

---
+++ python full stack/sql/task 1/11.sql
@@ -0,0 +1,11 @@
+create database sks;
+use sks;
+show databases;
+show tables;
+CREATE TABLE books(
+    bookid INT ,
+    title VARCHAR(100),
+    author varchar(100),
+    publish int
+    );
+    select * from books;
